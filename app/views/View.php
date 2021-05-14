0d90c98047776340963ef44d2ddad022688a3084
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-05-14 15:21:04 +0200

    fixed styling admin page

--- app/views/View.php
+++ app/views/View.php
@@ -132,20 +132,21 @@ class View
   {
     foreach ($products as $product) {
       $html = <<<HTML
-          <tr>
-            <th scope="row">$product[id]</th>
-            <td>$product[name]</td>
-            <td>$product[stock]</td>
-            <td>
-              <a href="index.php/products/update?id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
-                  <form method="post" action="index.php/products/delete" style="display: inline-block">
-                      <input  type="hidden" name="id" value="$product[id]"/>
-                      <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-                  </form>
-            </td>
-          </tr>
-        HTML;
-
+              <tr>
+                <th scope="row">$product[id]</th>
+                <td>$product[name]</td>
+                <td>$product[stock]</td>
+                <td>
+                  <a href="?page=admin/products/update&id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
+                  </td>
+                  <td>
+                    <form method="post" action="?page=admin/products/delete" style="display: inline-block">
+                        <input  type="hidden" name="id" value="$product[id]"/>
+                        <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
+                </td>
+              </tr>
+            HTML;
       echo $html;
     }
   }
