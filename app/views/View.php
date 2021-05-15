30e43701e578ca00c5ba021c5c265eef4ac5a248
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-15 17:06:31 +0200

    :art: Format and indent View.php

--- app/views/View.php
+++ app/views/View.php
@@ -6,40 +6,37 @@ class View
         include_once "app/views/partials/header.php";
     }
 
-  /**
-   * Receive null by default for create form, or data for update form
-   */
-  public function renderForm($data = null)
-  {
-    include_once 'app/views/partials/form.php';
-  }
-
-  public function renderCustomerProducts($products)
-  {
-    foreach ($products as $product) {
-      $this->renderOneCustomerProduct($product);
+    /**
+     * Receive null by default for create form, or data for update form
+     */
+    public function renderForm($data = null)
+    {
+        include_once "app/views/partials/form.php";
     }
-  }
 
-  public function renderOneCustomerProduct($product)
-  {
+    public function renderCustomerProducts($products)
+    {
+        foreach ($products as $product) {
+            $this->renderOneCustomerProduct($product);
+        }
+    }
 
-    $html = <<<HTML
-    <div class="col-md-3 mt-3">
-      <div class="card" style="width: 18rem;">
-        <img src="$product[image]" class="card-img-top p-3" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">$product[name]</h5>
-          <p class="card-text">$product[price] sek</p>
-          <a href="#" class="btn btn-primary">add to cart</a>
-        </div>
-      </div>
-    </div>
-
-    HTML;
-
-    echo $html;
-  }
+    public function renderOneCustomerProduct($product)
+    {
+        $html = <<<HTML
+            <div class="col-md-3 mt-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="$product[image]" class="card-img-top p-3" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">$product[name]</h5>
+                        <p class="card-text">$product[price] sek</p>
+                        <a href="#" class="btn btn-primary">add to cart</a>
+                    </div>
+                </div>
+            </div>
+        HTML;
+        echo $html;
+    }
 
     public function renderFooter()
     {
@@ -57,24 +54,24 @@ class View
         include_once "app/views/partials/footer.php";
     }
 
-    public function renderAdminProductCreatePage($brands, $categories)
+    public function renderAdminProductCreatePage()
     {
         $this->renderHeader("Admin Page - Create");
         $this->renderAdminHeader();
         echo '<a class="btn btn-secondary d-flex justify-content-center" href="?page=admin">Go back to product list</a></br>';
-        $this->renderForm(null, $brands, $categories);
+        $myArray = [
+            "name" => "Big",
+        ];
+        $this->renderForm($myArray);
         include_once "app/views/partials/footer.php";
     }
 
-    public function renderAdminProductUpdatePage(
-        $product_data,
-        $brands,
-        $categories
-    ) {
+    public function renderAdminProductUpdatePage($product_data)
+    {
         $this->renderHeader("Admin Page - Update");
         $this->renderAdminHeader();
         echo '<a class="btn btn-secondary d-flex justify-content-center" href="?page=admin">Go back to product list</a></br>';
-        $this->renderForm($product_data, $brands, $categories);
+        $this->renderForm($product_data);
         include_once "app/views/partials/footer.php";
     }
 
@@ -84,7 +81,27 @@ class View
         echo '<h2 class="text-center">Admin</h2>';
         echo '<h3 class="text-center">Nav placeholder</h3>';
     }
-    
+
+    public function renderProductsListStart()
+    {
+        $html = <<<HTML
+            <div class="row d-flex justify-content-center">
+                <div class="col-md-10">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Stock</th>
+                            <th scope="col">Edit</th>
+                            <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+        HTML;
+        echo $html;
+    }
+
     public function renderAdminPage($products)
     {
         $this->renderHeader("Admin Page - Products");
@@ -95,48 +112,19 @@ class View
         $this->renderProductsListEnd();
         include_once "app/views/partials/footer.php";
     }
-    
-        public function renderProductsListStart()
-        {
-            $html = <<<HTML
-                <div class="row d-flex justify-content-center">
-                    <div class="col-md-10">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Stock</th>
-                                <th scope="col">Edit</th>
-                                <th scope="col">Delete</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-            HTML;
-            echo $html;
-        }
-        
-    public function renderProducts($products)
+
+    public function renderCreatePage()
     {
-        foreach ($products as $product) {
-            $html = <<<HTML
-                <tr>
-                    <th scope="row">$product[id]</th>
-                    <td>$product[name]</td>
-                    <td>$product[stock]</td>
-                    <td>
-                        <a href="index.php/products/update?id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
-                            <form method="post" action="index.php/products/delete" style="display: inline-block">
-                                <input  type="hidden" name="id" value="$product[id]"/>
-                                <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-                            </form>
-                    </td>
-                </tr>
-            HTML;
-            echo $html;
-        }
+        $this->renderHeader("Admin Page - Create");
+        $this->renderAdminHeader();
+        echo '<a class="btn btn-secondary d-flex justify-content-center" href="?page=admin">Go back to product list</a></br>';
+        $myArray = [
+            "name" => "Big",
+        ];
+        $this->renderForm($myArray);
+        include_once "app/views/partials/footer.php";
     }
-        
+
     public function renderProductsListEnd()
     {
         $html = <<<HTML
@@ -146,5 +134,28 @@ class View
             </div>
         HTML;
         echo $html;
+    }
+
+    public function renderProducts($products)
+    {
+        foreach ($products as $product) {
+            $html = <<<HTML
+                <tr>
+                    <th scope="row">$product[id]</th>
+                    <td>$product[name]</td>
+                    <td>$product[stock]</td>
+                    <td>
+                        <a href="?page=admin/products/update&id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
+                    </td>
+                    <td>
+                        <form method="post" action="?page=admin/products/delete" style="display: inline-block">
+                            <input  type="hidden" name="id" value="$product[id]"/>
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            HTML;
+            echo $html;
+        }
     }
 }
