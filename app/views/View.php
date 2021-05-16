058abbee201fc8868574780e3f1105d15c67f9b4
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-16 16:05:15 +0200

    ♻️Refactor list methods so they can be reused

--- app/views/View.php
+++ app/views/View.php
@@ -55,9 +55,9 @@ class View
         $this->renderAdminHeader();
         $this->renderButton("Add new product", "?page=admin/products/create");
         $this->renderButton("Go to order list", "?page=admin/orders", false);
-        $this->renderProductsListStart();
-        $this->renderProducts($products);
-        $this->renderProductsListEnd();
+        $this->renderListStart(["#", "Name", "Stock", "Edit", "Delete"]);
+        $this->renderListItemsProducts($products);
+        $this->renderListEnd();
         include_once "app/views/partials/footer.php";
     }
 
@@ -85,7 +85,9 @@ class View
         $this->renderHeader("Admin - Order List");
         $this->renderAdminHeader();
         $this->renderButton("Go back to product list", "?page=admin", false);
-        echo "Placeholder for order list";
+        $this->renderListStart(["#", "Name", "Stock", "Edit", "Delete"]);
+        $this->renderListItemsOrders();
+        $this->renderListEnd();
         include_once "app/views/partials/footer.php";
     }
 
@@ -96,7 +98,7 @@ class View
         echo '<h3 class="text-center">Nav placeholder</h3>';
     }
 
-    public function renderProductsListStart()
+    public function renderListStart($column_name_array)
     {
         $html = <<<HTML
             <div class="row d-flex justify-content-center">
@@ -104,19 +106,40 @@ class View
                     <table class="table">
                         <thead>
                             <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Stock</th>
-                            <th scope="col">Edit</th>
-                            <th scope="col">Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
+        HTML;
+        foreach ($column_name_array as $column_name) {
+            $html .= "<th scope='col'>$column_name</th>";
+        }
+        $html .= <<<HTML
+                    </tr>
+                </thead>
+            <tbody>
         HTML;
         echo $html;
     }
 
-    public function renderProductsListEnd()
+    public function renderListItemsOrders()
+    {
+        $html = <<<HTML
+            <tr>
+                <th scope="row">product[id]</th>
+                <td>product[name]</td>
+                <td>product[stock]</td>
+                <td>
+                    <a href="?page=admin/products/update&id=product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
+                </td>
+                <td>
+                    <form method="post" action="?page=admin/products/delete" style="display: inline-block">
+                        <input  type="hidden" name="id" value="product[id]"/>
+                        <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        HTML;
+        echo $html;
+    }
+
+    public function renderListEnd()
     {
         $html = <<<HTML
                         </tbody>
@@ -127,7 +150,7 @@ class View
         echo $html;
     }
 
-    public function renderProducts($products)
+    public function renderListItemsProducts($products)
     {
         foreach ($products as $product) {
             $html = <<<HTML
