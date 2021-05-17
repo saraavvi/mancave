7c1afb7edc69cfa03cb5a0b8ec06933c78316d4e
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-05-17 16:01:34 +0200

    make separate header for customer and admin

--- app/views/View.php
+++ app/views/View.php
@@ -1,26 +1,24 @@
 <?php
 class View
 {
-    public function renderHeader($title)
-    {
-        include_once "app/views/partials/header.php";
+  public function renderHeader($title, $admin = false)
+  {
+    include_once "app/views/partials/head.php";
+    if ($admin) {
+      include_once "app/views/partials/adminNav.php";
+    } else {
+      include_once "app/views/partials/customerNav.php";
     }
+  }
 
-    public function renderFooter()
-    {
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderFooter()
+  {
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderAdminHeader()
-    {
-        echo '<h1 class="text-center">ManCave</h1>';
-        echo '<h2 class="text-center">Admin</h2>';
-        echo '<h3 class="text-center">Nav placeholder</h3>';
-    }
-
-    public function renderProductsListStart()
-    {
-        $html = <<<HTML
+  public function renderProductsListStart()
+  {
+    $html = <<<HTML
             <div class="row d-flex justify-content-center">
                 <div class="col-md-10">
                     <table class="table">
@@ -35,40 +33,70 @@ class View
                         </thead>
                     <tbody>
         HTML;
-        echo $html;
-    }
+    echo $html;
+  }
 
-    /**
-     * Receive null by default for create form, or data for update form
-     */
-    public function renderForm($brands, $categories, $data = null)
-    {
-        include_once "app/views/partials/form.php";
-    }
+  /**
+   * Receive null by default for create form, or data for update form
+   */
+  public function renderForm($brands, $categories, $data = null)
+  {
+    include_once "app/views/partials/form.php";
+  }
 
-    public function renderProductPage($products)
-    {
-        $this->renderHeader("mancave - products");
-        $this->renderCustomerProducts($products);
-        $this->renderFooter();
-    }
+  public function renderProductPage($products)
+  {
+    $this->renderHeader("mancave - products");
+    $this->renderCustomerProducts($products);
+    $this->renderFooter();
+  }
 
-    public function renderDetailPage($product)
-    {
-        $this->renderHeader("mancave - products");
-        $this->renderProductDetails($product);
-        $this->renderFooter();
-    }
+  public function renderDetailPage($product)
+  {
+    $this->renderHeader("mancave - products");
+    $this->renderProductDetails($product);
+    $this->renderFooter();
+  }
 
-    public function renderProductDetails($product)
-    {
-        //  Bara för att visa produkten just nu - byt ut detta mot vad vi vill visa på den här sidan.
-        $html = <<<HTML
+  public function renderProductDetails($product)
+  {
+
+    //  Bara för att visa produkten just nu - byt ut detta mot vad vi vill visa på den här sidan.
+    $html = <<<HTML
+            <div class="col-md-6 border">
+              <img src="$product[image]" class="img-fluid" alt="...">
+            </div>
+            <div class="col-md-4 border">
+              <h2>$product[name]</h2>
+              <p class="fs-1">$product[price] SEK</p>
+              <button class="btn btn-primary w-100">add to cart</button>
+              <div class="mt-3">
+                <p class="fw-bold">product information</p>
+                <p>$product[description]</p>
+              <div>
+              <div class="mt-3">
+                <p>$product[specification]</p>
+              <div>
+            </div>
+        HTML;
+    echo $html;
+  }
+
+  public function renderCustomerProducts($products)
+  {
+    foreach ($products as $product) {
+      $this->renderOneCustomerProduct($product);
+    }
+  }
+
+  public function renderOneCustomerProduct($product)
+  {
+    $html = <<<HTML
             <div class="col-md-3 mt-3">
                 <div class="card" style="width: 18rem;">
-                <a href="?page=products/details&id=$product[id]"> 
+                  <a href="?page=products/details&id=$product[id]"> 
                     <img src="$product[image]" class="card-img-top p-3" alt="...">
-                </a>
+                  </a>
                     <div class="card-body">
                         <h5 class="card-title">$product[name]</h5>
                         <p class="card-text">$product[price] sek</p>
@@ -77,176 +105,129 @@ class View
                 </div>
             </div>
         HTML;
-        echo $html;
-    }
+    echo $html;
+  }
 
-    public function renderCustomerProducts($products)
-    {
-        foreach ($products as $product) {
-            $this->renderOneCustomerProduct($product);
-        }
-    }
-
-    public function renderOneCustomerProduct($product)
-    {
-        // print_r($product['id']);
-        $html = <<<HTML
-            <div class="col-md-3 mt-3">
-                <div class="card" style="width: 18rem;">
-                <a href="?page=products/details&id=$product[id]"> 
-                    <img src="$product[image]" class="card-img-top p-3" alt="...">
-                </a>
-                    <div class="card-body">
-                        <h5 class="card-title">$product[name]</h5>
-                        <p class="card-text">$product[price] sek</p>
-                        <a href="#" class="btn btn-primary">add to cart</a>
-                    </div>
-                </div>
-            </div>
-        HTML;
-        echo $html;
-    }
-
-    public function renderButton($text, $href, $style = "primary")
-    {
-        $html = <<<HTML
+  public function renderButton($text, $href, $style = "primary")
+  {
+    $html = <<<HTML
             <div class="d-flex justify-content-center p-1">
                 <a class="btn btn-$style" href="$href">$text</a>
             </div>
         HTML;
-        echo $html;
-    }
+    echo $html;
+  }
 
-    public function renderAdminIndexPage($products)
-    {
-        $this->renderHeader("Admin Page - Products");
-        $this->renderAdminHeader();
-        $this->renderButton("Add new product", "?page=admin/products/create");
-        $this->renderButton(
-            "Go to order list",
-            "?page=admin/orders",
-            "secondary"
-        );
-        $this->renderListStart(["#", "Name", "Stock", "Edit", "Delete"]);
-        $this->renderListItemsProducts($products);
-        $this->renderListEnd();
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderAdminIndexPage($products)
+  {
+    $this->renderHeader("admin - home", true);
+    $this->renderButton("Add new product", "?page=admin/products/create");
+    $this->renderListStart(["#", "Name", "Stock", "Edit", "Delete"]);
+    $this->renderListItemsProducts($products);
+    $this->renderListEnd();
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderAdminProductCreatePage($brands, $categories, $errors)
-    {
-        $this->renderHeader("Admin Page - Create");
-        $this->renderAdminHeader();
-        $this->renderButton(
-            "Go back to product list",
-            "?page=admin",
-            "secondary"
-        );
-        $this->renderButton(
-            "Go to order list",
-            "?page=admin/orders",
-            "secondary"
-        );
-        $this->renderErrors($errors);
-        $this->renderForm($brands, $categories);
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderAdminProductCreatePage($brands, $categories, $errors)
+  {
+    $this->renderHeader("Admin Page - Create", true);
+    $this->renderButton(
+      "Go back to product list",
+      "?page=admin",
+      "secondary"
+    );
+    $this->renderErrors($errors);
+    $this->renderForm($brands, $categories);
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderAdminProductUpdatePage(
-        $brands,
-        $categories,
-        $product_data,
-        $errors = []
-    ) {
-        $this->renderHeader("Admin Page - Update");
-        $this->renderAdminHeader();
-        $this->renderButton(
-            "Go back to product list",
-            "?page=admin",
-            "secondary"
-        );
-        $this->renderErrors($errors);
-        $this->renderForm($brands, $categories, $product_data);
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderAdminProductUpdatePage(
+    $brands,
+    $categories,
+    $product_data,
+    $errors = []
+  ) {
+    $this->renderHeader("Admin Page - Update", true);
+    $this->renderButton(
+      "Go back to product list",
+      "?page=admin",
+      "secondary"
+    );
+    $this->renderErrors($errors);
+    $this->renderForm($brands, $categories, $product_data);
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderAdminOrderListPage(/* $orders */)
-    {
-        $this->renderHeader("Admin - Order List");
-        $this->renderAdminHeader();
-        $this->renderButton(
-            "Go back to product list",
-            "?page=admin",
-            "secondary"
-        );
-        $this->renderListStart([
-            "#",
-            "Date Placed",
-            "Customer Name",
-            "Status",
-            "Change Status",
-            "View Order",
-        ]);
-        $this->renderListItemsOrders();
-        $this->renderListEnd();
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderAdminOrderListPage(/* $orders */)
+  {
+    $this->renderHeader("Admin - Order List", true);
+    $this->renderListStart([
+      "#",
+      "Date Placed",
+      "Customer Name",
+      "Status",
+      "Change Status",
+      "View Order",
+    ]);
+    $this->renderListItemsOrders();
+    $this->renderListEnd();
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderAdminPage($products)
-    {
-        $this->renderHeader("Admin Page - Products");
-        $this->renderAdminHeader();
-        echo '<a class="btn btn-primary d-flex justify-content-center" href="?page=admin/products/create">Add new product</a></br>';
-        $this->renderProductsListStart();
-        $this->renderProducts($products);
-        $this->renderProductsListEnd();
-        include_once "app/views/partials/footer.php";
-    }
+  public function renderAdminPage($products)
+  {
+    $this->renderHeader("Admin Page - Products", true);
+    echo '<a class="btn btn-primary d-flex justify-content-center" href="?page=admin/products/create">Add new product</a></br>';
+    $this->renderProductsListStart();
+    $this->renderProducts($products);
+    $this->renderProductsListEnd();
+    include_once "app/views/partials/footer.php";
+  }
 
-    public function renderErrors($errors)
-    {
-        foreach ($errors as $message) {
-            echo "<div class='alert alert-danger' role='alert'>
-                $message
-            </div>";
-        }
+  public function renderErrors($errors)
+  {
+    foreach ($errors as $message) {
+      echo "<div class='alert alert-danger' role='alert'>
+    $message
+    </div>";
     }
+  }
 
-    public function renderListStart($column_name_array)
-    {
-        $html = <<<HTML
-            <div class="row d-flex justify-content-center">
-                <div class="col-md-10">
-                    <table class="table">
-                        <thead>
-                            <tr>
-        HTML;
-        foreach ($column_name_array as $column_name) {
-            $html .= "<th scope='col'>$column_name</th>";
-        }
-        $html .= <<<HTML
-                    </tr>
-                </thead>
-            <tbody>
-        HTML;
-        echo $html;
+  public function renderListStart($column_name_array)
+  {
+    $html = <<<HTML
+    <div class="row d-flex justify-content-center">
+        <div class="col-md-10">
+            <table class="table">
+                <thead>
+                    <tr>
+HTML;
+    foreach ($column_name_array as $column_name) {
+      $html .= "<th scope='col'>$column_name</th>";
     }
+    $html .= <<<HTML
+            </tr>
+        </thead>
+    <tbody>
+HTML;
+    echo $html;
+  }
 
-    public function renderListEnd()
-    {
-        $html = <<<HTML
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        HTML;
-        echo $html;
-    }
+  public function renderListEnd()
+  {
+    $html = <<<HTML
+                </tbody>
+            </table>
+        </div>
+    </div>
+HTML;
+    echo $html;
+  }
 
-    public function renderListItemsOrders(/* $orders */)
-    {
-        /* foreach ($products as $product) { */
-        $html = <<<HTML
+  public function renderListItemsOrders(/* $orders */)
+  {
+    /* foreach ($products as $product) { */
+    $html = <<<HTML
             <tr>
                 <th scope="row">8348</th>
                 <td>1/5 2021</td>
@@ -268,14 +249,14 @@ class View
                 </td>
             </tr>
         HTML;
-        echo $html;
-        /* } */
-    }
+    echo $html;
+    /* } */
+  }
 
-    public function renderListItemsProducts($products)
-    {
-        foreach ($products as $product) {
-            $html = <<<HTML
+  public function renderListItemsProducts($products)
+  {
+    foreach ($products as $product) {
+      $html = <<<HTML
                 <tr>
                     <th scope="row">$product[id]</th>
                     <td>$product[name]</td>
@@ -291,44 +272,44 @@ class View
                     </td>
                 </tr>
             HTML;
-            echo $html;
-        }
+      echo $html;
     }
+  }
 
-    public function renderProductsListEnd()
-    {
-        $html = <<<HTML
+  public function renderProductsListEnd()
+  {
+    $html = <<<HTML
 
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                </tbody>
+            </table>
+        </div>
+    </div>
 
-        HTML;
+HTML;
 
-        echo $html;
+    echo $html;
+  }
+
+  public function renderProducts($products)
+  {
+    foreach ($products as $product) {
+      $html = <<<HTML
+            <tr>
+                <th scope="row">$product[id]</th>
+                <td>$product[name]</td>
+                <td>$product[stock]</td>
+                <td>
+                <a href="?page=admin/products/update&id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
+                </td>
+                <td>
+                    <form method="post" action="?page=admin/products/delete" style="display: inline-block">
+                        <input  type="hidden" name="id" value="$product[id]"/>
+                        <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            HTML;
+      echo $html;
     }
-
-    public function renderProducts($products)
-    {
-        foreach ($products as $product) {
-            $html = <<<HTML
-                <tr>
-                    <th scope="row">$product[id]</th>
-                    <td>$product[name]</td>
-                    <td>$product[stock]</td>
-                    <td>
-                    <a href="?page=admin/products/update&id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
-                    </td>
-                    <td>
-                        <form method="post" action="?page=admin/products/delete" style="display: inline-block">
-                            <input  type="hidden" name="id" value="$product[id]"/>
-                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                HTML;
-            echo $html;
-        }
-    }
+  }
 }
---
+++ app/views/partials/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../styles/styles.css">
+</head>
+
+<body class="container">
