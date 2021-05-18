3bc5e38fe5e697435840bdc4f14eb54a05c6e72a
Maja Thunberg <majaneko@gmail.com>
2021-05-18 11:53:39 +0200

    Rename fn renderProducts -> renderListedProducts

--- app/views/View.php
+++ app/views/View.php
@@ -187,41 +187,17 @@ class View
 
     public function renderListStart($column_name_array)
     {
-        include_once "app/views/partials/listStart.php";
+        include_once "app/views/partials/list/listStart.php";
     }
 
     public function renderListEnd()
     {
-        $html = <<<HTML
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    HTML;
-        echo $html;
+        include_once "app/views/partials/list/listEnd.php";
     }
 
-    public function renderProducts($products)
+    public function renderListedProducts($products)
     {
-        foreach ($products as $product) {
-            $html = <<<HTML
-                <tr>
-                    <th scope="row">$product[id]</th>
-                    <td>$product[name]</td>
-                    <td>$product[stock]</td>
-                    <td>
-                    <a href="?page=admin/products/update&id=$product[id]" class="btn btn-sm btn-outline-primary">Edit</a>
-                    </td>
-                    <td>
-                        <form method="post" action="?page=admin/products/delete" style="display: inline-block">
-                            <input  type="hidden" name="id" value="$product[id]"/>
-                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-                HTML;
-            echo $html;
-        }
+        include_once "app/views/partials/list/productList.php";
     }
 
     /**
