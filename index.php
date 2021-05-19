10325b53b085a89960f136d1abbb5b0de9f2cb93
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-19 11:26:39 +0200

    📝Comment on structure

--- index.php
+++ index.php
@@ -36,3 +36,18 @@ $routes = array(
 
 $customer_login_controller = new CustomerLoginController($database, $view);
 $controller = new Controller($customer_login_controller, $order_model, $product_model, $customer_model, $view, $routes);
+
+/* Routern måste vara boss över två Controllers.
+new AdminController($controller_helper) och CustomerController.
+
+
+Dom två controllers har relevanta models injicerade.
+ControllerHelper klass där gemensamma resurser deklareras.
+
+
+new Router($admin_controller, $customer_controller) 
+
+1. initiera objekt av alla resurser som används
+2. initiera objekt av helperController som har gemensamma resurser
+3. initiera objekt av de två controllerna som är injecerade med helperController.
+4. initiera objekt av router som har de två åvan injicerade*/
