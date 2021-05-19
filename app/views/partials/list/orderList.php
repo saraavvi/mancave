715e10448782292b73cb987e389854fc924e92a6
Maja Thunberg <majaneko@gmail.com>
2021-05-19 08:56:41 +0200

    Fix href bug, change href for delete

--- app/views/partials/list/orderList.php
+++ app/views/partials/list/orderList.php
@@ -1,8 +1,5 @@
 <?php 
 foreach ($orders as $order) {
-    // current url path to append additional query params to (status)
-    //TODO: needs to be fixed = "/mancave/?page=admin/orders&id=1&status_id=1&id=3&status_id=2&id=1&status_id=2"
-    $uri = $_SERVER['REQUEST_URI'];
     $html = <<<HTML
         <tr>
             <th scope="row">$order[id]</th>
@@ -14,17 +11,17 @@ foreach ($orders as $order) {
                     Change Status
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a class="dropdown-item" href="{$uri}&id={$order['id']}&status_id=1">Draft</a></li>
-                    <li><a class="dropdown-item" href="{$uri}&id={$order['id']}&status_id=2">Shipped</a></li>
-                    <li><a class="dropdown-item" href="{$uri}&id={$order['id']}&status_id=3">Pending</a></li>
-                    <li><a class="dropdown-item" href="{$uri}&id={$order['id']}&status_id=4">Cancelled</a></li>
+                    <li><a class="dropdown-item" href="?page=admin/orders&id={$order['id']}&status_id=1">Draft</a></li>
+                    <li><a class="dropdown-item" href="?page=admin/orders&id={$order['id']}&status_id=2">Shipped</a></li>
+                    <li><a class="dropdown-item" href="?page=admin/orders&id={$order['id']}&status_id=3">Pending</a></li>
+                    <li><a class="dropdown-item" href="?page=admin/orders&id={$order['id']}&status_id=4">Cancelled</a></li>
                 </ul>
             </td>
             <td>
                 <a href="#" class="btn btn-sm btn-outline-primary">View Order</a>
             </td>
             <td>
-                <a href="{$uri}&id={$order['id']}&action=delete" class="btn btn-sm btn-outline-danger">Delete Order</a>
+                <a href="?page=admin/orders/delete&id={$order['id']}" class="btn btn-sm btn-outline-danger">Delete Order</a>
             </td>
         </tr>
     HTML;
