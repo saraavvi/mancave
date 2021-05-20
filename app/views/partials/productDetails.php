d5cfbfb25fa7dee15058d5058be34f222f6effe4
Antonio A R <antonio.axelsson@gmail.com>
2021-05-20 20:47:56 +0200

    fix detail img responsiveness, add padding to text

--- app/views/partials/productDetails.php
+++ app/views/partials/productDetails.php
@@ -1,7 +1,7 @@
-<div class="col-md-6 border">
-    <img src="<?= $product['image'] ?>" class="img-fluid p-4" alt="<?= $product['name'] ?>">
+<div class="col-xl-6 col-lg-11 col-md-10 border row justify-content-center">
+    <img src="<?= $product['image'] ?>" class="img-fluid p-4" alt="<?= $product['name'] ?>" style='max-height: 480px; max-width: 480px; object-fit: scale-down'>
 </div>
-<div class="col-md-4 border">
+<div class="col-xl-4 col-lg-11 col-md-10 border p-4">
     <h2><?= $product['name'] ?></h2>
     <p class="fs-1"><?= $product['price'] ?> SEK</p>
     <form action="#" method="POST">
