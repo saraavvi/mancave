19df538f2166e47c10caf75c8d00a20f6c0430b4
Antonio A R <antonio.axelsson@gmail.com>
2021-05-20 20:18:03 +0200

    remove desc. and spec. headers

--- app/views/partials/productDetails.php
+++ app/views/partials/productDetails.php
@@ -5,14 +5,12 @@
     <h2><?= $product['name'] ?></h2>
     <p class="fs-1"><?= $product['price'] ?> SEK</p>
     <form action="#" method="POST">
-        <button type="submit" name="add_to_cart" class="btn btn-primary w-100">add to cart</button>
+        <button type="submit" name="add_to_cart" class="btn btn-primary w-100">Add to Cart</button>
     </form>
     <div class="mt-3">
-        <p class="fw-bold">product information</p>
         <p><?= $product['description'] ?></p>
         <div>
             <div class="mt-3">
-                <p class="fw-bold">Specifications:</p>
                 <p><?= $product['specification'] ?></p>
                 <div>
                 </div>
