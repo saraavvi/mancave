851830a6d7dce74db028b0229de4d497d61b1ea9
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-05-21 08:16:25 +0200

    fix empty shoppinfg cart problemo

--- app/views/partials/head.php
+++ app/views/partials/head.php
@@ -9,6 +9,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="public/styles/styles.css">
 </head>
 
