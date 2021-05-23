27a537644956b5deb6000dbdb9a0f8f1bafcb539
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-23 14:38:27 +0200

    ♻️Breakout loginFormContent.php

--- app/views/partials/adminLogin.php
+++ app/views/partials/adminLogin.php
@@ -2,18 +2,7 @@
     <div class="col-md-6 col-sm-8 col-xs-12 p-4">
         <h3 class="modal-title text-center" id="loginModalLabel">Log in to your account:</h3>
         <form action="?page=admin/login" method="POST">
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
-            </div>
-            <div class="d-grid gap-2 col-6 mx-auto">
-                <button type="submit" class="btn btn-primary" type="button">Log In</button>
-            </div>
+            <?php include_once "loginFormContent.php"; ?>
         </form>
     </div>
 </div>
--- app/views/partials/loginModal.php
+++ app/views/partials/loginModal.php
@@ -8,19 +8,7 @@
             <div class="modal-body p-4">
                 <h3 class="modal-title text-center" id="loginModalLabel">Log in to your account:</h3>
                 <form action="?page=login" method="POST">
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password">
-                    </div>
-                    <div class="d-grid gap-2 col-6 mx-auto">
-                        <input type="hidden" name="current_page" value="<?= $_SERVER["QUERY_STRING"] ?>">
-                        <button type="submit" class="btn btn-primary" type="button">Log In</button>
-                    </div>
+                    <?php include_once "loginFormContent.php"; ?>
                 </form>
             </div>
             <div class="modal-footer bg-light d-flex justify-content-center">
