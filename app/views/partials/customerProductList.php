134aa2a2347d6ab7f13d7fbe0cb290ea3bb52e2d
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-05-24 08:15:33 +0200

    fix buttons on product page + some styling

--- app/views/partials/customerProductList.php
+++ app/views/partials/customerProductList.php
@@ -1,22 +1,21 @@
 <?php
 foreach ($products as $product) {
     echo "
-        <div class='col-xl-3 col-lg-4 col-md-5 col-sm-6 mt-3'>
-            <div class='card' style='height: 30rem;'>
+        <div class='col-xl-3 col-lg-4 col-md-5 col-sm-6 mt-3 mb-5'>
+            <div class='card bg-light border-0' style='min-height: 30vh; height: 100%;'>
                 <a href='?page=products/details&id=$product[id]'> 
                     <img src='$product[image]' class='card-img-top p-3' alt='$product[name]' style='height: 20rem; object-fit: contain;'>
                 </a>
-                <div class='card-body'>
+                <div class='card-body d-flex flex-column'>
                     <h5 class='card-title'>$product[name]</h5>
                     <p class='card-text'>$product[price] SEK</p>
-                    <form action='#' method='POST'>
+                    <form class='mt-auto' action='#' method='POST'>
                         <input type='hidden' name='product_id' value='$product[id]'>
                         <input type='hidden' name='product_name' value='$product[name]'>
                         <input type='hidden' name='current_page' value='$_SERVER[QUERY_STRING]'>
-                        <button type='submit' name='add_to_cart' class='btn btn-primary'>Add to Cart</button>
+                        <button type='submit' name='add_to_cart' class='btn btn-outline-dark'>Add to Cart</button>
                     </form>
                 </div>
             </div>
         </div>";
-
 }
